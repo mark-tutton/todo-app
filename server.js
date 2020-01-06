108eeaaf3237f2d4054144858f6e04cce86835fb
job_talk <job-talk@users.noreply.github.com>
2020-01-05 22:24:38 -0600

    adding update functionality

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ let mongodb = require('mongodb')
 let app = express()
 let db
 
+app.use(express.static('public'))
+
 let connectionString = 'mongodb+srv://todoAppUser:p19yyRF8uFHRpxrw@cluster0-pk0kc.mongodb.net/TodoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
 	db = client.db()
 	app.listen(3000)
 })
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
@@ -41,7 +44,7 @@ app.get('/', function(req, res) {
 			      	return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
 			      				 <span class="item-text">${item.text}</span>
 			      				 <div>
-			      				 	<button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+			      				 	<button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
 			      				    <button class="delete-me btn btn-danger btn-sm">Delete</button>
 			      				 </div>
 			      			</li>`
@@ -50,6 +53,8 @@ app.get('/', function(req, res) {
 			    
 			  </div>
 			  
+			  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+			  <script src="/browser.js"></script>
 			</body>
 			</html>
 		`)		
@@ -59,6 +64,12 @@ app.get('/', function(req, res) {
 app.post('/create-item', function(req, res) {
 	db.collection('items').insertOne({text: req.body.item}, function() {
 		res.redirect('/')
+	})
+})
+
+app.post('/update-item', function(req, res) {
+	db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+		res.send("Success")
 	})
 })
 
